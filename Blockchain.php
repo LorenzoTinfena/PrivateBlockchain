2282076a0b94f4eeb97713e142d46d9af61b661c
Lorenzo Tinfena <lorenzo.tinfena.personal@gmail.com>
2020-08-19 16:41:51 +0200

    add Blockchain.php

---
+++ Blockchain.php
@@ -0,0 +1,62 @@
+<?php
+class Blockchain
+{
+    private PDO $db;
+    public array $table;
+    public string $tableName;
+    public function __construct(PDO $db, string $tableName)
+    {
+        //FETCH DATA
+        $statement = $db->prepare("SELECT * FROM $tableName");
+        $statement->execute();
+        $this->table = $statement->fetchAll(PDO::FETCH_ASSOC);
+        $this->db = $db;
+        $this->tableName = $tableName;
+
+        date_default_timezone_set('UTC');
+    }
+    public function AddBlock(string $value)
+    {
+        //ADD NEW BLOCK
+        $valueHashed = BlockChain::sha256($value);
+        $timeStamp = date("Y-m-d H:i:s");
+        $query = "INSERT INTO $this->tableName (`Value`,`TimeStamp`, `Hash`, `ValueHashed`) VALUES
+        ('$value',
+        '$timeStamp','" . 
+        $this->ComputeHash($valueHashed, $timeStamp) . "', 
+        '$valueHashed')";
+
+        $stmt = $this->db->prepare($query);
+        if (!$stmt)
+            die("Error preparing db");
+        if (!$stmt->execute())
+            die("Error quering db");
+    }
+    private function ComputeHash(string $valueHashed, string $timeStamp)
+    {
+        //FETCH LAST BLOCK
+        $query = "SELECT Hash FROM $this->tableName ORDER BY TimeStamp DESC LIMIT 1";
+        $stmt = $this->db->prepare($query);
+        if (!$stmt)
+            die("Error preparing db");
+        if (!$stmt->execute())
+            die("Error quering db");
+        $row = $stmt->fetch(PDO::FETCH_UNIQUE);
+        if (!$row)
+            die("Eroor fetching last row");
+        //HASH
+        return BlockChain::sha256($valueHashed . $timeStamp . $row["Hash"]);
+    }
+    private static function sha256($value)
+    {
+        return hash("sha256", $value);
+    }
+}
+
+/*
+DB TABLE FORMAT
+    Value: text => text of the value
+    TimeStamp: timestamp => date of the timestamp of the block
+    Hash: tinytext => sha256(ValueHashed + TimeStamp + prev Hash)
+    ValueHashed: tinytext => sha256(Value)
+    */
